c2627414357dcdd7bbf2a9d893005456a969080c
heddaholm1 <99564542+heddaholm1@users.noreply.github.com>
2023-01-26 19:52:01 +0100

    Contact component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import './footer.css'
+import React from "react"
+import "./footer.css"
+import { FaFacebookF } from "react-icons/fa"
+import { FiInstagram } from "react-icons/fi"
+import { IoLogoTwitter } from "react-icons/io"
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<footer>
+			<a href="#top" className="footer__logo">
+				Hedda Holmen
+			</a>
+
+			<ul className="permalinks">
+				<li>
+					<a href="#top">Home</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#services">Services</a>
+				</li>
+				<li>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+
+			<div className="footer__socials">
+				<a href="#top">
+					<FaFacebookF />
+				</a>
+				<a href="#top">
+					<FiInstagram />
+				</a>
+				<a href="#top">
+					<IoLogoTwitter />
+				</a>
+			</div>
+
+			<div className="footer__copyright">
+				<small>&copy; Hedda N. Holmen</small>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
